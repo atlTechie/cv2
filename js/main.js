5f185e2ff6717e68d88ef6dd40ebfc18832ca7e5
Erika Wright <hey@ewright.me>
2021-06-14 18:22:27 -0400

    added work page with grid

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 gsap.from("#e", {
-  duration: 1.5,
+  duration: 1,
   x: -150,
   ease: "ease",
   opacity: 0,
 });
 
 gsap.from(".welcome-stuff", {
-  duration: 1.25,
-  scale: 0.5,
+  duration: 1.5,
+  y: 100,
   opacity: 0,
   delay: 0.5,
   stagger: 0.2,
-  ease: "elastic",
+  ease: "back",
 });
